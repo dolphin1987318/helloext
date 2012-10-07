08a4c9f4302454c2983a4dfec8ef5deaacc61d77
wxf <dog0120@163.com>
2012-10-07 12:28:10 +0800

    MVC architecture

---
+++ app/controller/Users.js
@@ -0,0 +1,47 @@
+Ext.define('AM.controller.Users', {
+    extend: 'Ext.app.Controller',
+
+    stores: ['Users'],
+    
+    views: [
+        'user.List',
+        'user.Edit'
+    ],
+    
+    models: ['User'],
+
+    init: function() {
+        this.control({
+            'userlist': {
+                render: this.onPanelRendered,
+                itemdblclick: this.editUser
+            },
+            'useredit button[action=save]': {
+                click: this.updateUser
+            }
+        });
+    },
+
+    editUser: function(grid, record) {
+        var view = Ext.widget('useredit');  //equals to  Ext.create('widget.useredit')
+
+        view.down('form').loadRecord(record);   //down function: accepts a ComponentQuery selector to quickly find any child component
+    },
+
+    updateUser: function(button) {
+        //console.log('clicked the Save button - id: ' + button.id);
+        var win    = button.up('window'),
+            form   = win.down('form'),
+            record = form.getRecord(),
+            values = form.getValues();
+
+        record.set(values);
+        win.close();
+        // synchronize the store after editing the record
+        this.getUsersStore().sync();
+    },
+
+    onPanelRendered: function() {
+        console.log('The panel was rendered');
+    }
+});
---
+++ app/store/Users.js
@@ -0,0 +1,18 @@
+Ext.define('AM.store.Users', {
+    extend: 'Ext.data.Store',
+    model: 'AM.model.User',
+    autoLoad: true,         //means the Store will ask its Proxy to load that data immediately.
+
+    proxy: {                //Proxies are the way to load and save data from a Store or a Model in Ext JS 4.  There are proxies for AJAX, JSON-P and HTML5 localStorage among others.
+        type: 'ajax',
+        api: {
+            read: 'data/users.json',
+            update: 'data/updateUsers.json'
+        },
+        reader: {           //The reader is responsible for decoding the server response into a format the Store can understand.
+            type: 'json',
+            root: 'users',
+            successProperty: 'success'
+        }
+    }
+});
